8f3b1ba32e0f5de48cd92b10508f664d439df7fa
Choza <rafael.choza@envistaco.com>
2025-04-07 11:31:10 -0600

    Change img location

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  base: '/mi-portafolio/',
+  plugins: [react()],
+});
