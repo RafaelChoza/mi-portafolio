a0a440a28653cbff20b88f5f7037ebff2647b13c
Choza <rafael.choza@envistaco.com>
2025-04-09 08:09:02 -0600

    add emailjs to form

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -1,74 +1,70 @@
-import { Fade } from "react-awesome-reveal";
-import Background from "../components/Background";
-import { useLanguage } from "../components/LanguageProvider";
+import React, { useRef, FormEvent } from 'react';
+import emailjs from '@emailjs/browser';
+import Background from '../components/Background';
+import { useLanguage } from '../components/LanguageProvider';
 
-
-export default function Contact() {
+export const ContactUs: React.FC = () => {
 
   const { language } = useLanguage();
+
+  const form = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!form.current) return;
+
+    emailjs
+      .sendForm('service_6ysuaei', 'template_dwdivc8', form.current, {
+        publicKey: 'un98RibMIRvOKMhsD',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
+  };
 
   return (
     <div className="relative text-white min-h-screen flex flex-col">
       <Background />
-      <Fade>
-        <form action="" className="flex flex-col items-center">
-          <label
-            htmlFor="name"
-            className="text-white text-2xl p-3"
-          >{language === 'es' ? 'Nombre' : 'Name'}</label>
-          <input
-            className="bg-white text-gray-600 p-3 border-6 border-gray-600 rounded-xl"
-            type="text" name="nombre"
-            placeholder={language === 'es' ? 'Tu nombre aqui' : 'Your name here'}
-          />
+      <form ref={form} onSubmit={sendEmail} className='flex flex-col items-center'>
+        <label htmlFor="name" className='text-white p-5 text-3xl'>{language === "es" ? "Nombre" : "Name"}</label>
+        <input className='bg-white w-1/2 p-5 text-black rounded-3xl' placeholder={language === "es" ? "Tu nombre aquí" : "Your name here"} type="text" name="user_name" id="name" required />
 
-          <label
-            htmlFor="correo"
-            className="text-white text-2xl p-3">{language === 'es' ? 'Correo' : 'Mail'}</label
-          >
-          <input
-            className="bg-white text-gray-600 p-3 border-6 border-gray-600 rounded-xl"
-            type="text" name="correo"
-            placeholder={language === 'es' ? 'Tu correo aqui' : 'Your mail here'}
-          />
+        <label htmlFor="email" className='text-white p-5 text-3xl'>{language === "es" ? "Correo" : "E-mail"}</label>
+        <input className='bg-white w-1/2 p-5 text-black rounded-3xl' placeholder={language === "es" ? "Tu correo aquí" : "Your e-mail here"} type="email" name="user_email" id="email" required />
 
-          <label
-            htmlFor="comentarios"
-            className="text-white text-2xl p-3">{language === 'es' ? 'Comentarios' : 'Comments'}</label
-          >
-          <textarea
-            className="bg-white text-gray-600 p-3 border-6 border-gray-600 rounded-xl mb-5"
-            name="comentarios"
-            placeholder={language === 'es' ? 'Tus comentarios aqui' : 'Your comments here'}
-          />
-          <button
-          className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300"
-        >
-          {language === 'es' ? 'Enviar' : 'Send'}
-        </button>
-        </form>
-        
+        <label htmlFor="message" className='text-white p-5 text-3xl'>{language === "es" ? "Comentarios" : "Comments"}</label>
+        <textarea className='bg-white w-1/2 p-5 text-black rounded-3xl' placeholder={language === "es" ? "Tus comentarios aquí" : "Your comments here"} name="message" id="message" required />
 
-        <h2 className="text-2xl md:text-3xl font-semibold animate-slide-left text-center">{language === 'es' ? 'Redes Sociales' : 'Social Media'}</h2>
-        <section className="flex flex-row justify-center items-center h-full">
-          <a href="https://www.linkedin.com/in/rafaelchoza-fullstackjava" target="_blank" rel="noopener noreferrer">
-            <img
-              src="https://www.vectorlogo.zone/logos/linkedin/linkedin-ar21.svg"
-              alt="linkedinLogo"
-              className="bg-white mx-10 rounded-2xl m-10 hover:scale-105 transform 
+        <input className='bg-gradient-to-r from-blue-500 to-purple-600 hover:from-purple-600 text-white p-5 rounded-md m-5' type="submit" value={language === "es" ? "Enviar" : "Send"} />
+      </form>
+      <h2 className="text-2xl md:text-3xl font-semibold animate-slide-left text-center">{language === "es" ? "Redes Sociales" : "Social Media"}</h2>
+      <section className="flex flex-row justify-center items-center h-full">
+        <a href="https://www.linkedin.com/in/rafaelchoza-fullstackjava" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://www.vectorlogo.zone/logos/linkedin/linkedin-ar21.svg"
+            alt="linkedinLogo"
+            className="bg-white mx-10 rounded-2xl m-10 hover:scale-105 transform 
             transition-transform duration-300"
-            />
-          </a>
-          <a href="https://www.github.com/rafaelchoza" target="_blank" rel="noopener noreferrer">
-            <img
-              src="https://www.vectorlogo.zone/logos/github/github-ar21.svg"
-              alt="GitHubLogo"
-              className="bg-white mx-10 rounded-2xl m-10 hover:scale-105 transform transition-transform
+          />
+        </a>
+        <a href="https://www.github.com/rafaelchoza" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://www.vectorlogo.zone/logos/github/github-ar21.svg"
+            alt="GitHubLogo"
+            className="bg-white mx-10 rounded-2xl m-10 hover:scale-105 transform transition-transform
             duration-300"
-            />
-          </a>
-        </section>
-      </Fade>
+          />
+        </a>
+      </section>
     </div>
-  )
-}
+
+  );
+};
+
+export default ContactUs;
