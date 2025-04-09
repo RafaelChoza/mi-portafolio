86c2e6373b3a910818189422337182b201d5d092
Rafael <166154431+RafaelChoza@users.noreply.github.com>
2025-04-08 22:00:06 -0600

    change images direction again

--- src/resources/projectsDb.ts
+++ src/resources/projectsDb.ts
@@ -3,7 +3,7 @@ const projects = [
         name: 'Pagina de Peliculas basada en una API',
         description: 'Esta pagina usa una REST API como base de datos para obtener la informacion de las peliculas. Tiene opcion de filtrar las peliculas. Este proyecto usa HTML, CSS, y Javascript',
         link: 'https://rafaelchoza.github.io/temaze-API-project/',
-        image: '/public/peliculones.jpg'
+        image: '/peliculones.jpg'
     },
     {
         name: 'Consultor de Criptomonedas',
@@ -15,61 +15,61 @@ const projects = [
         name: 'Consultor del Clima',
         description: 'Esta pagina usa una REST API para obtener la informacion de diferentes criptomonedas. Este proyecto usa ReactJS, Tailwind, TypeScript ans Zustand.',
         link: 'https://darling-pie-4eaa7e.netlify.app/',
-        image: '/src/img/clima.jpg'
+        image: '/public/clima.jpg'
     },
     {
         name: 'Administrador de pacientes de una Veterinaria',
         description: 'Esta pagina a llevar in control de los pacientes de una veterinaria. Este proyecto usa la dependencia React-Form, tambien usa ReactJS, Tailwind, TypeScript',
         link: 'https://elaborate-sprinkles-3449ff.netlify.app/',
-        image: '/src/img/veterinaria.jpg'
+        image: '/public/veterinaria.jpg'
     },
     {
         name: 'Contador de Calorias',
         description: 'Esta pagina lleva un control de las calorias consumidas y las calorias quemadas calculando un balance total. Este proyecto usa ReactJS, TypeScript y Tailwind',
         link: 'https://coruscating-mandazi-a8e899.netlify.app/',
-        image: '/src/img/calorias.jpg'
+        image: '/public/calorias.jpg'
     },
     {
         name: 'Planeador de Gastos',
         description: 'Esta pagina ayuda a controlar los gastos dado un presupuesto. Este proyecto usa ReactJS, Typescript y Tailwind',
         link: 'https://harmonious-mermaid-2caa99.netlify.app/',
-        image: '/src/img/gastosReact.jpg'
+        image: '/public/gastosReact.jpg'
     },
     {
         name: 'Planeador de Gastos (JavaScript)',
         description: 'Esta pagina ayuda a controlar los gastos dado un presupuesto. Este proyecto solo usa HTML, CSS, Javascript.',
         link: 'https://rafaelchoza.github.io/Budget-administrator-Javascript/',
-        image: '/src/img/gastosJavascript.jpg'
+        image: '/public/gastosJavascript.jpg'
     },
     {
         name: 'Tienda de Guitarras',
         description: 'Pagina de una tienda de Guitarras. Este proyecto solo usa ReactJS, Typescript y Tailwind',
         link: 'https://66bd55c9196ce933c05b2800--spiffy-capybara-7c0d6b.netlify.app/',
-        image: '/src/img/guitarStore.jpg'
+        image: '/public/guitarStore.jpg'
     },
     {
         name: 'Tienda deportiva',
         description: 'Esta es una pagina que simula una tienda de articulos deportivos. Este proyecto solo una HTML, CSS y Javascript',
         link: 'https://rafaelchoza.github.io/ecommerce-project-EBAC/',
-        image: '/src/img/sportStore.jpg'
+        image: '/public/sportStore.jpg'
     },
     {
         name: 'Calculo de Balance de Dinero',
         description: 'Esta pagina calcula un balance de dinero tomando en cuenta los depositos y los retiros de dinero. Este proyecto usa ReactJS, Tailwind y TypeScript',
         link: 'https://spontaneous-capybara-9a320a.netlify.app/',
-        image: '/src/img/gastosBalance.jpg'
+        image: '/public/gastosBalance.jpg'
     },
     {
         name: 'Calculo de Balance de Dinero con Python',
         description: 'Esta pagina calcula un balance de dinero tomando en cuenta los depositos y los retiros de dinero. Este proyecto usa Python, Django y Tailwind',
         link: 'https://rafaelchoza.pythonanywhere.com/',
-        image: '/src/img/gastosBalancePython.jpg'
+        image: '/public/gastosBalancePython.jpg'
     },
     {
         name: 'Recetas de bebidas basado en una REST API',
         description: 'Esta pagina te muestra las recetas de bebidas dando los ingredientes. Este proyecto usa ReactJS, Tailwind, Zustans, Zod, React-Router-Dom',
         link: 'https://gorgeous-sprite-70a302.netlify.app/',
-        image: '/src/img/bebidas.jpg'
+        image: '/public/bebidas.jpg'
     },
 ]
 
